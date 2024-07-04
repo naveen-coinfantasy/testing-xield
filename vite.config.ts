765a329e44019f9fba28da5ea95987e9715b414e
Naveen Kumar A M <naveen@coinfantasy.io>
2024-07-04 16:01:49 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,45 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+/// <reference types="vite/client" />
 
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+import { join, resolve } from "node:path";
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  build: {
-    outDir: "build",
-  },
   plugins: [react()],
+  envDir: join(__dirname),
+  envPrefix: ["VITE_"],
+  define: {
+    "import.meta.env.APP_VERSION": `"${process.env.npm_package_version}"`,
+  },
+  publicDir: resolve(__dirname, "public"),
+  root: resolve(__dirname),
+  build: {
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1024,
+    reportCompressedSize: false,
+    outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "src") },
+      { find: "~", replacement: resolve(__dirname, "public") },
+    ],
+  },
+  base: "/",
+  server: {
+    port: 3000,
+    strictPort: true,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://127.0.0.1:3080',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\//, ''),
+    //   },
+    // },
+  },
 });
