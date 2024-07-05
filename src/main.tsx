335d3443ca29739f8a8e125072136c4c059fd029
Naveen Kumar A M <naveen@coinfantasy.io>
2024-07-05 10:26:29 +0530

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import MetaMaskProvider from "./providers/MetamaskProvider.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <MetaMaskProvider>
         <App />
       </MetaMaskProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
